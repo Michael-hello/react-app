8ed8d242c09847bef1e47c76710cde27990c4b4b
Michael-hello <michael.huw.jones@gmail.com>
2019-11-15 16:50:47 +0000

    'initial'

---
+++ src/components/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './home.css';
+
+//see https://github.com/rossbulat/ts-react-formik-carousel/blob/master/src/FormCarousel/StageButton.tsx
+
+export const Home = () => {
+    return(
+        <div className="home-container">
+            <div className="home-box">
+                <h2>Michael Jones</h2>  
+                <h2>Software Dev</h2> 
+                <h2>North Wales</h2> 
+            </div>
+        </div>
+    ) 
+}
