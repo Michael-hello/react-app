0552f8157480357174f5abbfcee3e3165aa7eb01
Mike Jones <mjones@robertson-geo.com>
2025-06-04 13:10:58 +0100

    refactor test data out

---
+++ src/test.ts
@@ -0,0 +1,140 @@
+
+export const test_json_data = `{
+    "record": {
+        "locations": [
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744783291660"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744783320097"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744783350236"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744783979241"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744784038003"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744784077027"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744784085987"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744784623900"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744784652575"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744784885914"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744784914682"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744784944784"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744784974694"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744785004644"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744785034694"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744785064640"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744785094700"
+            },
+            {
+                "latitude": "30.6811716",
+                "longitude": "54.8638583",
+                "id": "test",
+                "time": "1744785124685"
+            },
+            {
+                "latitude": "30.685967",
+                "longitude": "54.8734087",
+                "id": "test",
+                "time": "1744785154767"
+            },
+            {
+                "latitude": "30.685967",
+                "longitude": "54.8734087",
+                "id": "test",
+                "time": "1744785421204"
+            },
+            {
+                "latitude": "30.685967",
+                "longitude": "54.8734087",
+                "id": "test",
+                "time": "1744785676544"
+            }
+        ]
+    },
+    "metadata": {
+        "id": "67f8c9fd8a456b7966871939",
+        "private": false,
+        "createdAt": "2025-04-15T15:38:25.787Z",
+        "name": "LocationLogger"
+    }
+}`
+
